7e83f5b27da21e2a4da0ebc3db3761ca3a3aac1e
vannhatadg118 <nguyenvannhatc8nh1@gmail.com>
2024-09-10 22:40:58 +0700

    Add files via upload
    github card

---
+++ config (1).js
@@ -0,0 +1,13 @@
+const nameGirl = 'meiling💝';
+const giftUrl = '.';
+const eventName = 'Chúc Mừng sinh nhật';
+const titleCard = '生日快樂💝🎁';
+const contentCard = 'Chúc em có một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau🍀♥️';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'bebii.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
